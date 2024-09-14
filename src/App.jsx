251c7d8a47090c93e2babaefbdcb562513f7b2ba
Kayode <dadavictory2000@gmail.com>
2024-09-14 09:09:06 +0100

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+function App() {
+  const url = "https://course-api.com/react-tours-project";
+
+  return (
+    <div className="min-w-screen min-h-screen bg-body-gray dark:bg-body-blue flex justify-center">
+      <Typography
+        variant="h1"
+        color="initial"
+      >
+        welcome to todays to tutorial
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
